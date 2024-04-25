7ca36e663ba4103f65f8876c78e27dc82512db4b
path-0f-misantrope <168031877+path-0f-misantrope@users.noreply.github.com>
2024-04-25 09:43:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from aiogram import Bot, Dispatcher, Router
 import config  
 import handlers
 import utils
-ALLOWED_UPDATES = ['message', 'edited_message']  
+ALLOWED_UPDATES = ['message', 'edited_message']  #допускает только выбранные события
 
 bot = Bot(token=config.token)  
 dp = Dispatcher() 
 
-dp.include_router(handlers.router)
+dp.include_router(handlers.router) #регистрирует диспетчеры
 dp.include_router(utils.utils_rt)  
 
 async def main():
-    await bot.delete_webhook(drop_pending_updates=True)
+    await bot.delete_webhook(drop_pending_updates=True) #не учитывает запросы до включения бота
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 if __name__ == '__main__':
